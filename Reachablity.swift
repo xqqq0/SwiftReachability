026b4f7d806915e073e84c3891429f1a510d6479
程序小白猿 <15301072780@163.com>
2016-12-08 11:53:52 +0800

    为了方便起见，我们创建两个类方法。 第一个方法建一个 reachability的实例来控制网络连接。 第二个方法用来检测当我们是否连接的是本地wifi。

--- Reachablity.swift
+++ Reachablity.swift
@@ -53,6 +53,26 @@ class Reachablity: NSObject {
             return nil
         }
     }
+    
+    // 类方法初始化 Reachablity 类
+    static func networkReachabilityForInternetConnection() -> Reachablity? {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        return Reachablity(hostAddress: zeroAddress)
+    }
+    
+    // 用来检测当我们是否连接的是本地wifi
+    static func networkReachabilityForLocalWiFi() -> Reachablity? {
+        var localWifiAddress = sockaddr_in()
+        localWifiAddress.sin_len = UInt8(MemoryLayout.size(ofValue: localWifiAddress))
+        localWifiAddress.sin_family = sa_family_t(AF_INET)
+        // IN_LINKLOCALNETNUM is defined inas 169.254.0.0 (0xA9FE0000).
+        localWifiAddress.sin_addr.s_addr = 0xA9FE0000
+        
+        return Reachablity(hostAddress: localWifiAddress)
+    }
+
 
     
 }
--- swift-Network_Reachability/ViewController.swift
+++ swift-Network_Reachability/ViewController.swift
@@ -19,28 +19,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    func test() {
-               
-        // 类方法初始化 Reachablity 类
-        static func networkReachabilityForInternetConnection() -> Reachablity? {
-            var zeroAddress = sockaddr_in()
-            zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
-            zeroAddress.sin_family = sa_family_t(AF_INET)
-            return Reachablity(hostAddress: zeroAddress)
-        }
-        
-        // 用来检测当我们是否连接的是本地wifi
-        static func networkReachabilityForLocalWiFi() -> Reachablity? {
-            var localWifiAddress = sockaddr_in()
-            localWifiAddress.sin_len = UInt8(MemoryLayout.size(ofValue: localWifiAddress))
-            localWifiAddress.sin_family = sa_family_t(AF_INET)
-            // IN_LINKLOCALNETNUM is defined inas 169.254.0.0 (0xA9FE0000).
-            localWifiAddress.sin_addr.s_addr = 0xA9FE0000
-            
-            return Reachablity(hostAddress: localWifiAddress)
-        }
-
-    }
 }
 
